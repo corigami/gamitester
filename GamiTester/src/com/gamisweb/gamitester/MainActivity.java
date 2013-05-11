03366306d03ca3730a4a2e4a02e08ebe73bcd6f7
Corigami <corey.willinger@gmail.com>
2013-05-11 15:46:09 -0400

    Cleaned Main activity

--- GamiTester/src/com/gamisweb/gamitester/MainActivity.java
+++ GamiTester/src/com/gamisweb/gamitester/MainActivity.java
@@ -1,29 +1,21 @@
 package com.gamisweb.gamitester;
 
 import java.io.*;
-
 import com.gamisweb.gamitester.R;
 import com.gamisweb.utility.*;
-
 import java.util.ArrayList;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.database.Cursor;
-import android.view.Menu;
 import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
-
 
 public class MainActivity extends Activity {
 
@@ -34,21 +26,16 @@ public class MainActivity extends Activity {
 	private String homeScreenText="This is the homescreen text";
 	private String selectedExam;
 	private SimpleCursorAdapter dataAdapter;
-	//private LinearLayout root;
-
-
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_main);
-		System.out.println("OnCreate Has been initiated");
-		//root = (LinearLayout)findViewById(R.id.activityMainLayout);
+
 	}
 
 	protected void onStart(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.splash_screen);	
+
 	}
 
 	@Override
@@ -82,8 +69,8 @@ public class MainActivity extends Activity {
 		consent = true;
 		setContentView(R.layout.activity_main);
 	}	
-	//creates listener onClick to tell the program what to do when button1 is clicked.
-	public void reviewButtonOnClick(View view) {
+
+	public void reviewButtonOnClick(View view) { 	//creates listener onClick to tell the program what to do when button1 is clicked.
 		try {
 			createDatabase();
 		} catch (IOException e) {
@@ -93,17 +80,12 @@ public class MainActivity extends Activity {
 
 	}
 
-	//creates listener onClick to tell the program what to do when button1 is clicked.
-	public void examButtonOnClick(View view){
-//		homeScreenLayout1.setText("You clicked the Exam Button");
-		//	Intent intent = new Intent(this, SelectExamActivity.class); //switching to single activity
-		selectExam();
 
-		//startActivityForResult(intent, 1);//switching to single activity
+	public void examButtonOnClick(View view){ 	//creates listener onClick to tell the program what to do when button1 is clicked.
+		selectExam();
 	}	
 
 	public void questionButtonOnClick(View view){
-//		homeScreenLayout1.setText("You clicked the Question Button");
 		Intent intent = new Intent(this, ShowQuestionsActivity.class);
 		intent.putExtra(EXTRA_MESSAGE, selectedExam);
 		startActivity(intent);
@@ -119,27 +101,16 @@ public class MainActivity extends Activity {
 		examHelper.deleteLocalDatabase();
 		examHelper.close();	
 		count=0;
-//		homeScreenLayout1.setText("You just deleted the Database");
 	}
 	private void createDatabase()throws IOException{
 		ExamDBHelper dbUtil = new ExamDBHelper(this);
-		/*	
-		EditText tempEditText = (EditText)findViewById(R.id.edit_message);
-		String file="";
-		if(tempEditText.equals(""))	file = "set2.txt";
-		else file = tempEditText.getText().toString();
-*/
 		AssetManager aM = this.getAssets();
 		InputStream fileToRead = aM.open("set1.txt");
 		InputStreamReader inputStreamReader = new InputStreamReader(fileToRead);
 		BufferedReader buff = new BufferedReader(inputStreamReader);
 		ArrayList<QInfo> questionArray = Io.getTextQuestions(buff);
-
 		QInfo questionStore = new QInfo();
-
-
 		dbUtil.open();
-
 		int i;
 		for(i=0; i<questionArray.size(); i++)
 		{
@@ -148,17 +119,11 @@ public class MainActivity extends Activity {
 					questionStore.getChoice(3), Integer.toString(questionStore.getCorrect()));
 		}			
 		dbUtil.createExam("Exam# "+count, "Author# "+count);
-
-
-		//	Cursor cursor = dbUtil.fetchAllExams();
-
 		dbUtil.close();
 		setHomeScreenLayout1("You added Exam #" + count + ", by Author #" + count + " to the database");
 		count++;
-
 		buff.close();
 		inputStreamReader.close();
-		//aM.close();
 
 	}
 	public void setHomeScreenLayout1(){
@@ -194,7 +159,7 @@ public class MainActivity extends Activity {
 				//Write your code if there's no result
 			}
 		}
-	}//onActivityResult
+	}
 
 	public void selectExam(){
 
@@ -223,29 +188,21 @@ public class MainActivity extends Activity {
 		String[] columns = new String[]
 				{
 			//examHelper.getAuthor(),examHelper.getTitle()
-
-
 			examHelper.KEY_TITLE,
 			examHelper.KEY_AUTHOR
 				};
+		
+		int[] to = new int[] {R.id.examTitle, R.id.examAuthor}; // the XML defined views which the data will be bound to
 
-		// the XML defined views which the data will be bound to
-		int[] to = new int[] {R.id.examTitle, R.id.examAuthor};
-
-		// create the adapter using the cursor pointing to the desired data as well as the layout information
-		ListView examListView = (ListView) findViewById(R.id.exam_listview);
+		
+		ListView examListView = (ListView) findViewById(R.id.exam_listview); // create the adapter using the cursor pointing to the desired data as well as the layout information
 		dataAdapter = new SimpleCursorAdapter(this, R.layout.exam_list_entries_layout, cursor, columns, to, 0); 
-
-		// Assign adapter to ListView
-		examListView.setAdapter(dataAdapter);
+		examListView.setAdapter(dataAdapter); // Assign adapter to ListView
 		examListView.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> listView, View view, int position, long id) {
-				// Get the cursor, positioned to the corresponding row in the result set
-				Cursor cursor = (Cursor) listView.getItemAtPosition(position);
-
-				// Get the selected exam from this row in the database.
-				selectedExam = cursor.getString(cursor.getColumnIndexOrThrow("Title"));
+				Cursor cursor = (Cursor) listView.getItemAtPosition(position); // Get the cursor, positioned to the corresponding row in the result set
+				selectedExam = cursor.getString(cursor.getColumnIndexOrThrow("Title")); // Get the selected exam from this row in the database.
 				setContentView(R.layout.activity_main);
 				
 			}
@@ -254,5 +211,3 @@ public class MainActivity extends Activity {
 		examHelper.close();
 	}
 }
-
-//    
