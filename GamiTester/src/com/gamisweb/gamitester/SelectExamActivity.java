3f2ab624c2d6011fae7ee4c24e8ac5a50d1e47a7
Corigami <corey.willinger@gmail.com>
2013-05-11 22:25:25 -0400

    cleaned code

--- GamiTester/src/com/gamisweb/gamitester/SelectExamActivity.java
+++ GamiTester/src/com/gamisweb/gamitester/SelectExamActivity.java
@@ -24,71 +24,52 @@ public class SelectExamActivity extends Activity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.exam_listview_layout);
-	//    Intent intent = getIntent();
-//	    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-		
-		
-		  //Generate ListView from SQLite Database
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.exam_listview_layout);
 		displayListView();
-
-		//examHelper.close();
-
 	}
 	private void displayListView() {
 		ExamDBHelper examHelper = new ExamDBHelper(this);
 		examHelper.open();	 
-	  Cursor cursor = examHelper.fetchAllExams();
-		  
+		Cursor cursor = examHelper.fetchAllExams();
 
-		  // The desired columns to be bound
-		  @SuppressWarnings("static-access")
-		  String[] columns = new String[]
+		@SuppressWarnings("static-access")
+		String[] columns = new String[]
 				{
-				  //examHelper.getAuthor(),examHelper.getTitle()
-				  
-				  
-				  examHelper.KEY_TITLE,
-				  examHelper.KEY_AUTHOR
+			examHelper.KEY_TITLE,
+			examHelper.KEY_AUTHOR
 				};
-		 
-		  // the XML defined views which the data will be bound to
-		  int[] to = new int[] 
-			  { 
-				  R.id.examTitle,
-				  R.id.examAuthor
-			  };
-		 
-		  // create the adapter using the cursor pointing to the desired data 
-		  //as well as the layout information
-		  ListView examListView = (ListView) findViewById(R.id.exam_listview);
-		  dataAdapter = new SimpleCursorAdapter(this, R.layout.exam_list_entries_layout, cursor, columns, to, 0); //swapped null for cursor
-		  // Assign adapter to ListView
-		  examListView.setAdapter(dataAdapter);
 
-		 
-		 
-		  examListView.setOnItemClickListener(new OnItemClickListener() {
-		   @Override
-		   public void onItemClick(AdapterView<?> listView, View view, 
-		     int position, long id) {
-		   // Get the cursor, positioned to the corresponding row in the result set
-		   Cursor cursor = (Cursor) listView.getItemAtPosition(position);
-		   
-		   // Get the selected exam from this row in the database.
-		   String examSelected = cursor.getString(cursor.getColumnIndexOrThrow("Title"));
-		   System.out.println(examSelected);
+		// the XML defined views which the data will be bound to
+		int[] to = new int[] 
+				{ 
+				R.id.examTitle,
+				R.id.examAuthor
+				};
 
-		 
-		   
-		   Intent returnIntent = new Intent();
-		   returnIntent.putExtra("result",examSelected);
-		   setResult(RESULT_OK,returnIntent);     
-		   finish();
-		   }
-			 
-		  });
+		// create the adapter using the cursor pointing to the desired data 
+		//as well as the layout information
+		ListView examListView = (ListView) findViewById(R.id.exam_listview);
+		dataAdapter = new SimpleCursorAdapter(this, R.layout.exam_list_entries_layout, cursor, columns, to, 0); //swapped null for cursor
+		examListView.setAdapter(dataAdapter); 		  // Assign adapter to ListView
 
-		}
+		examListView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> listView, View view, 
+					int position, long id) {
+				// Get the cursor, positioned to the corresponding row in the result set
+				Cursor cursor = (Cursor) listView.getItemAtPosition(position);
+
+				// Get the selected exam from this row in the database.
+				String examSelected = cursor.getString(cursor.getColumnIndexOrThrow("Title"));
+				System.out.println(examSelected);
+				Intent returnIntent = new Intent();
+				returnIntent.putExtra("result",examSelected);
+				setResult(RESULT_OK,returnIntent);     
+				finish();
+			}
+
+		});
+
+	}
 }
